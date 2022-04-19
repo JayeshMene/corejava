2042826ac7f5e244cf4855f4b79f20a407f3a6c0
JAYU <JAYU@DESKTOP-L8R6G59>
2022-04-19 21:06:46 +0530

    First Commit

---
+++ project1/src/project1/Project1.java
@@ -0,0 +1,25 @@
+package project1;
+
+public class Project1 {
+ int yearofpurchase;
+ String model;
+ double cost;
+ public void disp1()
+ {
+	 System.out.println("year of purchase" + 2001);
+ }
+  public void disp2()
+  {
+	  System.out.println("model" + 1999);
+	  System.out.println("cost"+ 200000);
+  }
+  
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  Project1 hai =new Project1();
+  Project1 bye =new Project1();
+  hai.disp1();
+  bye.disp2();
+	}
+
+}
