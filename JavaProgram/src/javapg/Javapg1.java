8f80da32375fef03502a89666cb80f141aefa29f
JAYU <JAYU@DESKTOP-L8R6G59>
2022-04-22 20:45:14 +0530

    First Commit

---
+++ JavaProgram/src/javapg/Javapg1.java
@@ -0,0 +1,10 @@
+package javapg;
+
+public class Javapg1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
---
+++ JavaProgram/src/module-info.java
@@ -0,0 +1,2 @@
+module JavaProgram {
+}
