cb9340fcd1ec8db2822b49b5dd8824a6a023e6ef
JAYU <JAYU@DESKTOP-L8R6G59>
2022-04-25 11:38:45 +0530

    First Commit

---
+++ JavaProgram/src/javapg/Hierachical.java
@@ -0,0 +1,48 @@
+package javapg;
+class One1
+{
+void disp1()
+{
+System.out.println("One");
+}
+}
+class Two1 extends One1
+{
+void disp2()
+{
+System.out.println("Two");
+}
+}
+class Three1 extends One1
+{
+void disp3()
+{
+System.out.println("Three");
+}
+}
+class Four1 extends One1
+{
+void disp4()
+{
+System.out.println("Four");
+}
+}
+public class Hierachical {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Four1 r1 = new Four1();
+		r1.disp1(); r1.disp4();
+		
+		Three1 r2 = new Three1();
+		r2.disp1(); r2.disp3();
+		
+		Two1 r3 = new Two1();
+		r3.disp1(); r3.disp2();
+		
+		One1 r4 = new One1();
+		r4.disp1();
+	}
+
+}
