92d582d9ec99163584e41e9579d232c3ffa0bcc9
JAYU <JAYU@DESKTOP-L8R6G59>
2022-04-26 12:45:10 +0530

    First Commit

---
+++ JavaProgram/src/javapg/Buffer.java
@@ -0,0 +1,15 @@
+package javapg;
+import java.io.*;
+public class Buffer {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter your Name: ");
+		String str = br.readLine();
+		System.out.println("Your name is   "+ str);
+
+
+}
+}
