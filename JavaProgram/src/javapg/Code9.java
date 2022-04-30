740f7945e1d665888ca3f1739c58cf83a242cc66
JAYU <JAYU@DESKTOP-L8R6G59>
2022-04-30 16:59:57 +0530

    First Commit

---
+++ JavaProgram/src/javapg/Code9.java
@@ -0,0 +1,19 @@
+package javapg;
+
+public class Code9 {		//DECISION MAKING WITH OPERATORS 
+
+	public static void main(String[] args) {
+		
+		int x = 23, y = 32;
+		
+	if (x>=y)
+	{
+		System.out.println("The Condition is False");
+	}
+	else 
+	{
+		System.out.println("The Condition is True");
+	}
+	}
+
+}
