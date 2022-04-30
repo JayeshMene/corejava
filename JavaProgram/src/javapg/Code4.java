801b7bd5d1f53d17aee4e738a6d6e8ecef0b3d93
JAYU <JAYU@DESKTOP-L8R6G59>
2022-04-30 16:33:29 +0530

    first Commit

---
+++ JavaProgram/src/javapg/Code4.java
@@ -0,0 +1,13 @@
+package javapg;
+
+public class Code4 {
+
+	public static void main(String[] args) {
+		boolean var= false;
+		System.out.println(var);
+		
+		char var1 = '\u00A5';
+		System.out.println(var1);
+	}
+
+}
