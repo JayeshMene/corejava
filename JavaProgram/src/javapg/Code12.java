4966714212d97c450bb5b950f56903df5a0bbd0d
JAYU <JAYU@DESKTOP-L8R6G59>
2022-04-30 17:33:34 +0530

    First Commit

---
+++ JavaProgram/src/javapg/Code12.java
@@ -0,0 +1,20 @@
+package javapg;
+
+public class Code12 {
+
+	public static void main(String[] args) {
+		
+		int x = 3;
+		
+	//	x+=5;	//	x=x+5;
+		x-=5;	//	x=x-5;
+	//	x*=5;	//	x=x*5;
+	//	x/=3;	//	x=x/5;
+    //	x%=4;	// x=x%5; 
+		
+		System.out.println(x);
+				
+
+	}
+
+}
