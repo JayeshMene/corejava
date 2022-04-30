ca8787b962106768aab86a37cf1cd3b3e69f6a2e
JAYU <JAYU@DESKTOP-L8R6G59>
2022-04-30 16:55:33 +0530

    First Commit

---
+++ JavaProgram/src/javapg/Code8.java
@@ -0,0 +1,18 @@
+package javapg;
+
+public class Code8 {	 // DECISION MAKING WITH IF ELSE STATEMENT
+
+	public static void main(String[] args) {
+		
+		int x = 6;
+	if (x!=5)
+	{
+		System.out.println("Value of x is not 5");
+	}
+	else 
+	{
+		System.out.println("Value of x is 5");
+	}
+	}
+
+}
