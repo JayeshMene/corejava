4ba7f0d9b1c28aa13887824f05a965153134667d
JAYU <JAYU@DESKTOP-L8R6G59>
2022-04-30 16:50:08 +0530

    First Commit

---
+++ JavaProgram/src/javapg/Code7.java
@@ -0,0 +1,14 @@
+package javapg;
+
+public class Code7 {		// ARITHMATIC OPERATORS
+
+	public static void main(String[] args) {
+		
+		int x = 10, y = 20;
+		System.out.println(x+y);
+		System.out.println(--x);
+		System.out.println(++y);
+
+	}
+
+}
