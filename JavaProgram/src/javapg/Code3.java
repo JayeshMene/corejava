7d3628caddb33c5ceb7b55a555ddc36cefa54fa9
JAYU <JAYU@DESKTOP-L8R6G59>
2022-04-30 16:24:38 +0530

    First Commit

---
+++ JavaProgram/src/javapg/Code3.java
@@ -0,0 +1,21 @@
+package javapg;
+
+public class Code3 {
+
+	public static void main(String[] args) {
+		 
+	/*	 int value1 = 9/2;
+		 float value2 = 10f/5f;
+		 double value3 = 10d/4d;
+		 System.out.println("value1 = "+ value1);
+		 System.out.println("value2 = "+ value2);
+		 System.out.println("value3 = "+ value3);  */
+		 
+		 int marks = 567;
+		 double percentage = marks * 0.36f;
+		 System.out.println("Percentage = "+ percentage);
+		 
+
+	}
+
+}
