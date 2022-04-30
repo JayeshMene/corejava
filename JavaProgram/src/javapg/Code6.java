5fa721987fbabb9f715cf1ebe0d858bc760168ad
JAYU <JAYU@DESKTOP-L8R6G59>
2022-04-30 16:45:38 +0530

    First Commit

---
+++ JavaProgram/src/javapg/Code6.java
@@ -0,0 +1,13 @@
+package javapg;
+
+public class Code6 {		// TYPECASTING
+
+	public static void main(String[] args) {
+	
+		float f = 10.532f;
+		long l = (long)f;
+		System.out.println(l);
+
+	}
+
+}
