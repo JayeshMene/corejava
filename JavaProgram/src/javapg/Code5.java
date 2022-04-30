09e715590a5fb55c5bffda0004279ff00f1ca00c
JAYU <JAYU@DESKTOP-L8R6G59>
2022-04-30 16:37:16 +0530

    First Commit

---
+++ JavaProgram/src/javapg/Code5.java
@@ -0,0 +1,9 @@
+package javapg;
+
+public class Code5 {		// STRING VARIABLE
+
+	public static void main(String[] args) {
+		String var = new String ("Hello World");
+		System.out.println(var);
+	}
+}
