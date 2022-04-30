18e3ad895c913e8ab86fb50dbb138d17c487384d
JAYU <JAYU@DESKTOP-L8R6G59>
2022-04-30 17:46:48 +0530

    First Commit

---
+++ JavaProgram/src/javapg/Code13.java
@@ -0,0 +1,23 @@
+package javapg;
+
+public class Code13 {		// MAKING DECISION WITH IF ELSE
+
+	public static void main(String[] args) {
+		
+		int x = 5;
+		
+	   if (x==5)
+	   {
+		   System.out.println("X is Equal to 5");
+	   }
+	   else if (x>5)
+	   {
+		   System.out.println("X is Greater than 5");
+	   }
+	   else 
+	   {
+		   System.out.println("X is smaller than 5");
+	   }
+	}
+
+}
