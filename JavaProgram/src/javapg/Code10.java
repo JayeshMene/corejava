c83444ce45cce01138ec9874b681655c174f92f9
JAYU <JAYU@DESKTOP-L8R6G59>
2022-04-30 17:15:43 +0530

    First Commit

---
+++ JavaProgram/src/javapg/Code10.java
@@ -0,0 +1,30 @@
+package javapg;
+
+public class Code10 {	// DECISON MAKING WITH OPERATORS
+
+	public static void main(String[] args) {
+	/*	int x=2, y=3;
+	if (!(x<y) || (x==y))
+	{
+		System.out.println("Condition is True");
+	}
+	else 
+	{
+		System.out.println("Condition is False");
+	} */
+
+	 
+		
+		int ageofBoy = 20;
+		int ageofGirl = 17;
+		
+	if ((ageofBoy>=21) && (ageofGirl>=18))
+	{
+		System.out.println("Ready to get Married!!");
+	}
+	else
+	{
+		System.out.println("Wait for it Kid!!");
+	}
+	}		
+}
