f51489a600a89fbaa1c41f8dc4919de1521968ae
JAYU <JAYU@DESKTOP-L8R6G59>
2022-04-30 17:53:44 +0530

    First Commit

---
+++ JavaProgram/src/javapg/Code14.java
@@ -0,0 +1,25 @@
+package javapg;
+
+public class Code14 {		// SWITCH CASE IN JAVA FOR INT
+
+	public static void main(String[] args) {
+		
+		int x=3;
+		
+		switch(x)
+		{
+		case 1 :
+			System.out.println("The Value of X is One");
+			break;
+		case 2 :
+			System.out.println("The Value of X is Two");
+			break;
+		case 3 : 
+			System.out.println(" The Value of X is Three");
+			break;
+		default:
+			System.out.println("The Value of X is other than 1,2,3");
+		}
+	}
+
+}
