c2d7cb1dd1dd692c7df6d1b4474905e9f326f642
JAYU <JAYU@DESKTOP-L8R6G59>
2022-05-01 16:52:24 +0530

    first Commit

---
+++ JavaProgram/src/javapg/Code17.java
@@ -0,0 +1,14 @@
+package javapg;
+
+public class Code17 {		// FOR LOOP
+	public static void main (String[] args)
+	{
+		//for (int i = 1; i<=1000 ; i++) 
+		for (int i=1000; i>=1; i--)
+		{
+			System.out.print("value of i :");
+			System.out.println(i);
+		}
+	}
+
+}
