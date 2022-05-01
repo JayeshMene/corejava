f0d009cb9e9616894dab84dc0d7ef1aae6f28652
JAYU <JAYU@DESKTOP-L8R6G59>
2022-05-01 17:09:55 +0530

    First Commit

---
+++ JavaProgram/src/javapg/Code20.java
@@ -0,0 +1,17 @@
+package javapg;
+
+public class Code20 {	//DO WHILE LOOP
+
+	public static void main(String[] args) {
+		
+		int i = 0;
+		do
+		{
+			System.out.println(i);
+			i++;
+		
+		} while (i<=1);
+
+	}
+
+}
