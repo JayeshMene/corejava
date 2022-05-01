1bede9acff27fd89a26c6e464121790e1f6bcad1
JAYU <JAYU@DESKTOP-L8R6G59>
2022-05-01 17:39:13 +0530

    First Commit

---
+++ JavaProgram/src/javapg/Code_21.java
@@ -0,0 +1,18 @@
+package javapg;
+
+public class Code_21 {		//BREAK AND CONTINUE
+
+	public static void main(String[] args) {
+		for (int i=4; i<=10; i++)
+		{
+			if (i==1)
+			{
+				//continue;	
+				break;
+			}
+			System.out.println(i);
+		}
+
+	}
+
+}
