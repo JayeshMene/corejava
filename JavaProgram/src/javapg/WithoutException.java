7fac2efb4ca2b1feb84f6dfd98c083fdc65bd733
JAYU <JAYU@DESKTOP-L8R6G59>
2022-05-10 14:12:56 +0530

    First Commit

---
+++ JavaProgram/src/javapg/WithoutException.java
@@ -0,0 +1,13 @@
+package javapg;
+
+public class WithoutException {		//Without Exception
+
+	public static void main(String[] args) {
+		
+		int d=0;
+		int a=42/d;
+		System.out.println("Will not printed");
+
+	}
+
+}
