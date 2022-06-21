7c506843826e35acff690a014e99fd4abed2ed08
JAYU <JAYU@DESKTOP-L8R6G59>
2022-06-21 13:53:01 +0530

    First Commit

---
+++ Hib_jpa_Operations/src/com/tns/entities/student.java
@@ -0,0 +1,34 @@
+package com.tns.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="student")
+public class student implements Serializable
+
+{	
+	private static final long serialVersionUID = 1L;
+
+	//public static final long serialversionUID=1L;
+	@Id
+	private int studentid;
+	private String name;
+	
+	public int getStudentid() {
+		return studentid;
+	}
+	public void setStudentid(int studentid) {
+		this.studentid = studentid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
