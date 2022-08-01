0e03c98ae381ebd2cfebf42bbc830c9706985312
JAYU <JAYU@DESKTOP-L8R6G59>
2022-08-01 09:14:15 +0530

    First Commit

---
+++ JavaProgram/src/exceptions/MultiCatch.java
@@ -0,0 +1,25 @@
+package exceptions;
+
+public class MultiCatch { 		//Multiple Try Catch blocks
+
+	public static void main(String[] args) {
+		
+		try {
+			int a = args.length;
+			System.out.println("a=" +a);
+			int b=42/a;
+			int c[] = {1};
+			c[42] = 99;
+		} catch (ArithmeticException e) {
+			System.out.println("Divide by zero :" +e);
+		}
+		catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Array Index OOB :" +e);
+		}
+		catch (Exception e) {
+			System.out.println("Generic Excepyion :" +e);
+		}
+		System.out.println("After Try/catch blocks");
+	}
+
+}
---
+++ JavaProgram/src/exceptions/NestedTryCatch.java
@@ -0,0 +1,27 @@
+package exceptions;
+
+public class NestedTryCatch {		//Nested Try Catch Block
+
+	public static void main(String[] args) {
+		
+		try {
+			int a =args.length;
+			int b =10/a;
+			System.out.println("a= "+a);
+			try {
+				if (a==1)
+					a=a/(a-a);
+				if (a==2) {
+					int c[] = {1};
+					c[42] = 99;
+				}
+			}catch (ArrayIndexOutOfBoundsException e) {
+				System.out.println("Array Index OOB :" +e);}
+			}catch (ArithmeticException e) {
+				System.out.println("Divide by Zero : "+e);}
+			}
+		
+
+	}
+
+
