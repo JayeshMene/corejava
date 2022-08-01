a243232042a66bfee6ad14e565a28d2c04814441
JAYU <JAYU@DESKTOP-L8R6G59>
2022-08-01 10:15:37 +0530

    First Commit

---
+++ JavaProgram/src/statickey/StaticMet.java
@@ -0,0 +1,29 @@
+package statickey;
+
+class Abc2
+{
+	static int add()	//Static Method	
+	{
+		int x = 10;
+		int y = 20;
+		int z = x+y;
+		return z;
+	}
+}
+class Xyz2
+{
+	void disp()		//local method 
+	{
+		int a = Abc2.add(); //Calling Static method
+		System.out.println("Addition = " + a);
+	}
+}
+public class StaticMet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Xyz2 r = new Xyz2();
+		r.disp();
+	}
+
+}
