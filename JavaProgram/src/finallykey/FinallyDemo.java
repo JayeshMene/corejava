a6d42dfd5da75320c8a25335188160ad353a6212
JAYU <JAYU@DESKTOP-L8R6G59>
2022-08-01 10:13:18 +0530

    First Commit

---
+++ JavaProgram/src/finallykey/FinallyDemo.java
@@ -0,0 +1,38 @@
+package finallykey;
+
+public class FinallyDemo {		//Finally Demo program
+
+	static void procA() {
+		try {
+			System.out.println("Inside ProcA");
+			throw new RuntimeException("Demo");
+		} finally {
+			System.out.println("ProcA is Finally");
+		}
+	}
+	static void procB() {
+		try {
+			System.out.println("Inside ProcB");
+			return;
+		} finally {
+			System.out.println("Procb is finally");
+		}
+	}
+	static void procC() {
+		try {
+			System.out.println("Inside ProcC");
+			
+		} finally {
+			System.out.println("ProcC is Finally");
+		}
+	}
+	public static void main(String[] args) {
+		try {
+			procA();
+		} catch (Exception e) {
+			System.out.println("Exception caught");
+		}procB(); procC();
+
+	}
+
+}
