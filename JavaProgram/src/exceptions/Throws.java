e7532158868a6176d2f84a1edd69f83b99ceb4a3
JAYU <JAYU@DESKTOP-L8R6G59>
2022-08-01 09:08:39 +0530

    First Commit

---
+++ JavaProgram/src/exceptions/Throws.java
@@ -0,0 +1,15 @@
+package exceptions;
+
+public class Throws {		//Exception in thread by throws keyword
+	
+		static void checkAge(int age) throws ArithmeticException {
+		if (age < 18) {
+		throw new ArithmeticException("Access denied - You must be at least 18 years old.");
+		} else {
+		System.out.println("Access granted - You are old enough!");
+		}
+		}
+		public static void main(String[] args) {
+		checkAge(15);
+		}
+}
